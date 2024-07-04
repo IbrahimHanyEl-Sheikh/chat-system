9c69739e2a5c7888ead923e75c323147a3359e44
IbrahimHanyEl-Sheikh <ibrahim.hany96@hotmail.com>
2024-07-04 10:09:10 +0000

    Auto formatting

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::ChatsController < ApplicationController
   before_action :set_application, only: [:create, :show, :index]
   before_action :set_chat, only: [:show]
+
+  # GET /chats
   def index
     @chats = @application.chats
     render json: @chats,  :except=> [:id, :application_id]
   end
+
+  # GET /chats/1
+  def show
+    render json: @chat
+  end
+
+  # POST /chats
   def create
 		chat_number = generate_chat_number(@application.token)
 		chat = @application.chats.new(number: chat_number,application_token: @application.token)
@@ -16,23 +25,22 @@ class Api::V1::ChatsController < ApplicationController
 		end
 	end
 
-	def show
-			render json: @chat
-	end
   private
-    def set_chat
-      @chat = @application.chats.find_by(number: params[:number]) if @application
-      unless @chat
-        render json: { error: "Chat not found" }, status: :not_found
-      end
-    end
 
+    # Use callbacks to share common setup or constraints between actions.
     def set_application
       @application = Application.find_by(token: params[:application_token])
       unless @application
         return render(json: { "error": 'Application not Found' }, status: :not_found)
       end
       puts "application founded"
+    end
+
+    def set_chat
+      @chat = @application.chats.find_by(number: params[:number]) if @application
+      unless @chat
+        render json: { error: "Chat not found" }, status: :not_found
+      end
     end
 
     # Only allow a list of trusted parameters through.
