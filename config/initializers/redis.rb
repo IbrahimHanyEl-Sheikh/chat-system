9251d4e60ccc5d2ed2b4ec3b87c1d32e6dd62331
IbrahimHanyEl-Sheikh <ibrahim.hany96@hotmail.com>
2024-07-04 22:41:08 +0000

    handle race condition in redis queries

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,25 +1,24 @@
 require 'redis'
 $redis = Redis.new(url: ENV["REDIS_URL"])
+
 # method to generate chat_number using redis
 def generate_chat_number(application_token)
-  puts "entered generate chat number method"
   # Construct a unique Redis key for storing chat numbers for each application
   redis_key = "chat_number_application_token_#{application_token}"
 
-  # Use Redis INCR command to increment the chat number
-  chat_number = $redis.incr(redis_key)
-
-  # Return the incremented chat number
-  chat_number
+  $redis_lock.lock(redis_key, 2000) do |locked|
+    chat_number = $redis.get(redis_key)
+    $redis.set(redis_key, chat_number.to_i + 1)
+    return chat_number
+  end
 end
+
 def generate_message_number(chat_number, application_token)
-  puts "entered generate message number method"
   # Construct a unique Redis key for storing message numbers for each application
   redis_key = "message_number_chat_number#{chat_number}_application_token_#{application_token}"
-
-  # Use Redis INCR command to increment the message number
-  message_number = $redis.incr(redis_key)
-
-  # Return the incremented message number
-  return message_number
+  $redis_lock.lock(redis_key, 2000) do |locked|
+  message_number = $redis.get(redis_key)
+    $redis.set(redis_key, message_number.to_i + 1)
+    return message_number
+  end
 end
